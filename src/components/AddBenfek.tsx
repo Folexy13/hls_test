6858fddfc2623214bbf4c9f24d78952cabb6c803
Folexy13 <folajimiopeyemisax13@gmail.com>
2025-03-02 09:22:30 +0100

    ..

--- src/components/AddBenfek.tsx
+++ src/components/AddBenfek.tsx
@@ -34,7 +34,7 @@ function AddBenfek() {
       <div className="md:max-w-7xl w-11/12 mx-auto">
         <button
             onClick={() => navigate('/dashboard')}
-            className="mb-6 flex items-center gap-2 text-white rounded bg-green-400 px-6 py-1 text-gray-600 hover:text-gray-900"
+            className="mb-6 flex items-center gap-2 text-white rounded bg-green-400 px-6 py-1  hover:text-gray-900"
         >
           <ArrowLeft className="w-5 h-5" />
           Back to Dashboard
